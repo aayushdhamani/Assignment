0af6360746d991c82b5b3f1037f9aba2bd62c860
Lakshya <iamlakshya18@gmail.com>
2024-03-05 00:54:28 +0530

    table

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import {Row, Col} from 'react-bootstrap'
+import companiesData from './Data/data';
+import Table from './Components/Table';
 
 const DashBoard = () => {
   return (
@@ -8,6 +10,9 @@ const DashBoard = () => {
         <Row>
             <Col md={3}>
             <Sidebar/>
+            </Col>
+            <Col md ={9}>
+            <Table companiesData={companiesData}></Table>
             </Col>
         </Row>
       
